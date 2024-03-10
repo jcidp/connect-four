0dce19d28300aeac57febfcb7a5eb74d30dfc924
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-09 19:44:16 -0600

    Add game over checks

--- lib/board.rb
+++ lib/board.rb
@@ -11,15 +11,28 @@ class Board
   def display_board
     5.downto(0) do |row|
       puts(board.each.reduce("|") do |str, lane|
-        str + " #{lane[row] || ' '} |"
+        str + " #{lane[row] ? lane[row].encode('utf-8') : '  '} |"
       end)
     end
+    puts "   1    2    3    4    5    6    7"
   end
 
   def add_to_lane(marker, lane)
     return unless valid_lane? lane
 
     board[lane].push(marker)
+  end
+
+  def valid_lane?(lane)
+    lane.between?(0, 6) && board[lane].length < 6
+  end
+
+  def full?
+    board.all? { |lane| lane.length == 6 }
+  end
+
+  def won?(marker)
+    vertical_win?(marker) || horizontal_win?(marker) || diagonal_win?(marker)
   end
 
   private
@@ -30,10 +43,48 @@ class Board
     reset_board
   end
 
-  def valid_lane?(lane)
-    lane.between?(0, 6) && board[lane].length < 6
+  def vertical_win?(marker)
+    board.each do |lane|
+      lane.each.reduce(0) do |count, e|
+        new_count = e == marker ? count + 1 : 0
+        return true if new_count == 4
+
+        new_count
+      end
+    end
+    false
+  end
+
+  def horizontal_win?(marker)
+    6.times do |row|
+      7.times.reduce(0) do |count, col|
+        new_count = board.dig(col, row) == marker ? count + 1 : 0
+        return true if new_count == 4
+
+        new_count
+      end
+    end
+    false
+  end
+
+  def diagonal_win?(marker)
+    up_right_starts = [[0, 2], [0, 1], [0, 0], [1, 0], [2, 0], [3, 0]]
+    down_right_starts = [[0, 3], [0, 4], [0, 5], [1, 5], [2, 5], [3, 5]]
+    up_right_starts.any? { |start| line_match?(marker, start) } ||
+      down_right_starts.any? { |start| line_match?(marker, start, "down") }
+  end
+
+  def line_match?(marker, start, direction = "up")
+    x = start[0]
+    y = start[1]
+    count = 0
+    while x.between?(0, 6) && y.between?(0, 5)
+      count = board.dig(x, y) == marker ? count + 1 : 0
+      return true if count == 4
+
+      x += 1
+      y = direction == "up" ? y + 1 : y - 1
+    end
+    false
   end
 end
-
-my_board = Board.new
-my_board.display_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -69,6 +69,108 @@ describe Board do
       end
     end
   end
+
+  describe "#full?" do
+    context "when board isn't full" do
+      it "returns false" do
+        expect(test_board).not_to be_full
+      end
+    end
+
+    context "when board is full" do
+      before do
+        test_board.instance_variable_set(:@board, Array.new(7) { %w[x o x o x o] })
+      end
+
+      it "returns true" do
+        expect(test_board).to be_full
+      end
+    end
+  end
+
+  describe "#won?" do
+    context "when the marker hasn't won" do
+      before do
+        test_board.instance_variable_set(:@board,
+                                         [
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[o x o x o x],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o]
+                                         ])
+      end
+
+      it "returns false" do
+        result = test_board.won? "x"
+        expect(result).to be false
+      end
+    end
+
+    context "when the marker wins vertically" do
+      before do
+        test_board.instance_variable_set(:@board, [%w[x o], %w[x o], ["x"], %w[x x x x], %w[x o], ["o"], ["x"]])
+      end
+
+      it "returns true" do
+        result = test_board.won? "x"
+        expect(result).to be true
+      end
+    end
+
+    context "when the marker wins horizontally" do
+      before do
+        test_board.instance_variable_set(:@board, [%w[x o], %w[x o], ["x"], %w[x o x o x o], %w[x o], ["o"], ["x"]])
+      end
+
+      it "returns true" do
+        result = test_board.won? "x"
+        expect(result).to be true
+      end
+    end
+
+    context "when the marker wins in an upward-right diagonal" do
+      before do
+        test_board.instance_variable_set(:@board,
+                                         [
+                                           %w[o o x o x o],
+                                           %w[x x x o x o],
+                                           %w[x o x o x o],
+                                           %w[o x o x o x],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o]
+                                         ])
+      end
+
+      it "returns true" do
+        result = test_board.won? "x"
+        expect(result).to be true
+      end
+    end
+
+    context "when the marker wins in an downward-right diagonal" do
+      before do
+        test_board.instance_variable_set(:@board,
+                                         [
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[x o x o x o],
+                                           %w[o x o x o x],
+                                           %w[x o o o x o],
+                                           %w[x o x x x o],
+                                           %w[x o x o x o]
+                                         ])
+      end
+
+      it "returns true" do
+        result = test_board.won? "x"
+        expect(result).to be true
+      end
+    end
+  end
 end
 
 # rubocop:enable Metrics/BlockLength
